74cabe5d74ec264479424af3389fcb49d1cf3e36
Jana Jovicic <jana.jovicic755@gmail.com>
2019-02-26 21:41:07 +0100

    Napravljena prva verzija mreze i kreiran model nad trening podacima

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import keras
 import os
 import random
 import skimage.data
@@ -11,7 +12,9 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
-from keras.optimizers import SGD
+#from keras.optimizers import SGD
+from keras import optimizers
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
 from keras import backend as K
 
 
@@ -20,6 +23,8 @@ from keras import backend as K
 # (samples, rows, cols, channels)
 K.set_image_data_format('channels_last')
 
+train_data_dir = "./dataset/train"
+test_data_dir = "./dataset/test"
 
 
 def load_data(data_dir):
@@ -39,15 +44,13 @@ def load_data(data_dir):
 
 
 
-train_data_dir = "./dataset/train"
-test_data_dir = "./dataset/test"
-
 images, classes = load_data(train_data_dir)
 
-IMG_SIZE = 32  # 32x32 images
+IMG_SIZE = 64  # 64x64 images
 NUM_CLASSES = len(set(classes))
 
 print("Number of classes: {0}\nNumber of images for training: {1}".format(NUM_CLASSES, len(images)))
+
 
 
 def display_images_and_classes(images, labels):
@@ -71,7 +74,92 @@ def display_images_and_classes(images, labels):
     plt.suptitle('Types of traffic signs in dataset')
     plt.show()
 
-display_images_and_classes(images, classes)
+
+
+#display_images_and_classes(images, classes)
 
 for image in images[:5]:
     print("shape: {0}, min: {1}, max: {2}".format(image.shape, image.min(), image.max()))
+    
+    
+# Postavljamo da sve slike budu istih dimenzija: 64x64
+images64 = [skimage.transform.resize(image, (IMG_SIZE, IMG_SIZE), mode='constant')
+                for image in images]
+#display_images_and_classes(images64, classes)
+
+images = np.array(images64)
+classes = np.array(classes)
+#print(classes[3])
+
+# Za klasu i postavljamo 1 na i-to mesto, a na ostala 0
+classes = np.eye(NUM_CLASSES, dtype='uint8')[classes]
+#print(classes[3])
+
+print("classes shape: ", classes.shape, "\nimages shape: ", images.shape)
+
+
+
+
+
+def cnn_model():
+    
+    model = Sequential()
+
+    # Konvolucija i sazimanje 1
+    model.add(Conv2D(filters = 32, kernel_size = (3, 3), padding='same', input_shape=(IMG_SIZE, IMG_SIZE, 3), data_format="channels_last", activation='relu'))
+    #model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.2))
+
+    # Konvolucija i sazimanje 2
+    model.add(Conv2D(filters = 32, kernel_size = (3, 3), padding='same', activation='relu'))
+    #model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    # Konvolucija i sazimanje 3
+    model.add(Conv2D(filters = 64, kernel_size = (3, 3), padding='same', activation='relu'))
+    #model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.2))
+
+    """
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+    """
+    
+    # Transformacija 3D slike u 1D vektor 
+    model.add(Flatten())
+    model.add(Dense(64)) # viseslojni perceptron
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5)) # deaktiviramo polovinu neurona kako ne bi doslo do preprilagodjavanja
+    model.add(Dense(NUM_CLASSES))  # output layer ima onoliko neurona koliko ima klasa
+    model.add(Activation('softmax'))
+
+    model.summary()
+    
+    return model
+
+
+
+batch_size = 64
+epochs = 20
+lr = 0.01
+
+model = cnn_model()
+
+# optimizacija pomocu gradijentnog spusta
+sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+#learning rate
+def lr_schedule(epoch):
+    return lr * (0.1 ** int(epoch / 10))
+
+model.fit(images, classes,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_split=0.2,
+          callbacks=[LearningRateScheduler(lr_schedule), ModelCheckpoint('model.h5', save_best_only=True)])

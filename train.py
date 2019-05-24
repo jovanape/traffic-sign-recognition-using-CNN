8a9b2b0041e278cdca4ed9aafe542ecfe8c00c9e
jovanape <32679363+jovanape@users.noreply.github.com>
2019-05-24 04:08:16 +0200

    Dodat AlexNet model (poglavlje), grafici, kod
    Dodat AlexNet model (poglavlje), grafici, kod

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import keras
-import os, sys
+import os
 import random
 import warnings
 import skimage.data
@@ -17,7 +17,6 @@ from keras import optimizers
 from keras.callbacks import LearningRateScheduler, ModelCheckpoint
 from keras import backend as K
 from timeit import default_timer as timer
-from keras.layers.normalization import BatchNormalization
 
 from utility import *
 
@@ -26,15 +25,9 @@ from utility import *
 # (samples, rows, cols, channels)
 K.set_image_data_format('channels_last')
 
+
 # Da ne izbacuje warninge. Zakomentarisati po potrebi.
 warnings.filterwarnings("ignore")
-
-if len(sys.argv) != 2:
-        print("""Usage:
-        For training with model_1: python3 train.py -M
-        For training with LeNet5: python3 train.py -L
-        For training with AlexNet: python3 train.py -A""")
-        sys.exit(0)
 
 images, classes = load_data(train_data_dir)
 
@@ -44,101 +37,34 @@ print("Number of classes: {0}\nNumber of images for training: {1}".format(NUM_OF
 
 
 # Da vidimo koje sve klase postoje u trening skupu podataka.
+# Zakomentarisati ako nas ne zanima.
 display_images_and_classes(images, classes)
+
+#for image in images[:5]:
+    #print("shape: {0}, min: {1}, max: {2}".format(image.shape, image.min(), image.max()))
+    
     
 # Postavljamo da sve slike budu istih dimenzija: IMG_SIZE x IMG_SIZE 
 images_resized = [skimage.transform.resize(image, (IMG_SIZE, IMG_SIZE), mode='constant')
                 for image in images]
+#display_images_and_classes(images_resized, classes)
 
 images = np.array(images_resized)
 classes = np.array(classes)
+#print(classes[26])
 
 # Za klasu i postavljamo 1 na i-to mesto, a na ostala 0
 classes = np.eye(NUM_OF_CLASSES, dtype='uint8')[classes]
+#print(classes[26])
 
 
+#print("classes shape: ", classes.shape, "\nimages shape: ", images.shape)
 
-def alexNet():
+
+def cnn_model():
     
     model = Sequential()
-
-    # 1. kovolucioni sloj
-    model.add(Conv2D(filters=3, input_shape=(IMG_SIZE, IMG_SIZE, 3), kernel_size=(3,3), padding='valid', data_format="channels_last"))
-    model.add(Activation('relu'))
-    # Pooling - agregacija
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(1,1), padding='valid'))
-    # Batch Normalisation - normalizacija (pre nego sto se prosledi narednom sloju)
-    model.add(BatchNormalization())
-
-    # 2. kovolucioni sloj
-    model.add(Conv2D(filters=16, kernel_size=(3,3), strides=(1,1), padding='valid'))
-    model.add(Activation('relu'))
-    # Pooling - agregacija
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-    # Batch Normalisation - normalizacija
-    model.add(BatchNormalization())
-
-    # 3. kovolucioni sloj
-    model.add(Conv2D(filters=16, kernel_size=(3,3), strides=(1,1), padding='valid'))
-    model.add(Activation('relu'))
-    # Batch Normalisation - normalizacija
-    model.add(BatchNormalization())
-
-    # 4. kovolucioni sloj
-    model.add(Conv2D(filters=3, kernel_size=(3,3), strides=(1,1), padding='valid'))
-    model.add(Activation('relu'))
-    # Batch Normalisation - normalizacija
-    model.add(BatchNormalization())
-
-    # 5. kovolucioni sloj
-    model.add(Conv2D(filters=3, kernel_size=(3,3), strides=(1,1), padding='valid'))
-    model.add(Activation('relu'))
-    # Pooling - agregacija
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-    # Batch Normalisation - normalizacija
-    model.add(BatchNormalization())
-
-
-    # Sloj za poravnanje
-    model.add(Flatten())
-    # 1. Dense sloj
-    model.add(Dense(units = 120))
-    model.add(Activation('relu'))
-    # Dodajemo Dropout da sprecimo overfitting
-    model.add(Dropout(0.4))
-    # Batch Normalisation - normalizacija
-    model.add(BatchNormalization())
-
-    # 2. Dense sloj
-    model.add(Dense(84))
-    model.add(Activation('relu'))
-    # Dodajemo Dropout
-    model.add(Dropout(0.4))
-    # Batch Normalisation - normalizacija
-    model.add(BatchNormalization())
-
-    # 3. Dense sloj
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    # Dodajemo Dropout
-    model.add(Dropout(0.4))
-    # Batch Normalisation - normalizacija
-    model.add(BatchNormalization())
-
-    # Izlazni sloj
-    model.add(Dense(NUM_OF_CLASSES))
-    model.add(Activation('softmax'))
-
-    model.summary()
-    return model
-
-
-
-
-def model_1():
-    
-    model = Sequential()
-    
+    ''''
     model.add(Conv2D(filters = 32, kernel_size = (3, 3), padding='same', input_shape=(IMG_SIZE, IMG_SIZE, 3), data_format="channels_last", activation='relu'))
     model.add(Conv2D(filters = 32, kernel_size = (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -153,16 +79,7 @@ def model_1():
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(NUM_OF_CLASSES, activation='softmax'))
-    
-    model.summary()
-    return model
-
-
-
-
-def leNet5():
-    
-    model = Sequential()
+    '''
     
     model.add(Conv2D(filters=6, kernel_size=(3, 3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3), data_format="channels_last"))
     model.add(AveragePooling2D())
@@ -171,88 +88,68 @@ def leNet5():
     model.add(AveragePooling2D())
 
     model.add(Flatten())
+
     model.add(Dense(units=120, activation='relu'))
+
     model.add(Dense(units=84, activation='relu'))
+
     model.add(Dense(units=NUM_OF_CLASSES, activation = 'softmax'))
     
+    
     model.summary()
+    
     return model
 
 
 
-def main():
+batch_size = 32    # broj trening podataka u jednoj iteraciji
+epochs = 20
+lr = 0.01          # learning rate
 
-    batch_size = 32    # broj trening podataka u jednoj iteraciji
-    epochs = 20
-    lr = 0.01          # learning rate
+timer_start = timer()
 
-    timer_start = timer()
-        
-        
-    if(sys.argv[1] == "-M"):
+model = cnn_model()
 
-        model = model_1()
-        # optimizacija pomocu stohastickog gradijentnog spusta
-        sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
-        model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-        
-    elif(sys.argv[1] == "-L"):
-        
-        model = leNet5()
-        model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
-        
-        
-    elif(sys.argv[1] == "-A"):
-        
-        model = alexNet()
-        model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
-        
-    
-    else:
-        print("""Usage:
-        For training with model_1: python3 train.py -M
-        For training with LeNet5: python3 train.py -L
-        For training with AlexNet: python3 train.py -A""")
-        sys.exit(0)
-    
-
-    
-
-    def lr_schedule(epoch):
-        return lr * (0.1 ** int(epoch / 10))
-
-    history = model.fit(images, classes,
-            batch_size=batch_size,
-            epochs=epochs,
-            validation_split=0.2,
-            callbacks=[LearningRateScheduler(lr_schedule), ModelCheckpoint('model.h5', save_best_only=True)])
-
-    timer_end = timer()
-
-    elapsed_time = timer_end - timer_start
-    print("Elapsed time: {0} seconds".format(elapsed_time))
-
-    # izlistavanje svih podataka
-    #print(history.history.keys())
-
-    # preciznost (accuracy)
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-
-    # podaci o promasaju (loss)
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
+model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
 
 
-if __name__ == '__main__':
-    main()
+# optimizacija pomocu gradijentnog spusta
+#sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+
+#model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+def lr_schedule(epoch):
+    return lr * (0.1 ** int(epoch / 10))
+
+history = model.fit(images, classes,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_split=0.2,
+          callbacks=[LearningRateScheduler(lr_schedule), ModelCheckpoint('model.h5', save_best_only=True)])
+
+timer_end = timer()
+
+elapsed_time = timer_end - timer_start
+print("Elapsed time: {0} seconds".format(elapsed_time))
+
+# izlistavanje svih podataka
+#print(history.history.keys())
+
+# preciznost (accuracy)
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# podaci o promasaju (loss)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
